05351d68ee45a46a2b389c7c5ad387c45f8918a5
danielhbbarbosa <danihenrique2007@gmail.com>
2024-10-16 19:23:01 -0300

    update categorias

--- categorias.php
+++ categorias.php
@@ -4,6 +4,13 @@
     <button>Enviar</button>
 </form> 
 
-<?php>
-    echo $_GET['nome_categoria'];
-<?>
+<?php
+    //echo $_GET['nome_categoria'];
+
+    if(isset($_GET['nome_categoria'])){
+        echo "Categoria: ".$_GET['nome_categoria'];
+    } 
+    else {
+        echo "Categoria não encontrada";
+    }
+?>
